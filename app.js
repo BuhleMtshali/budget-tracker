ff5ae8b31dc169512c138850dfdc12b084bf1278
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-09-08 21:56:52 +0200

    updated the index file

--- app.js
+++ app.js
@@ -3,4 +3,24 @@ const addBtn = document.getElementById("add-btn");
 addBtn.addEventListener("click", addItem);
 //start of function
 
-function addItem() {}
+function addItem() {
+  let inputField = `
+        <div class="salary">
+          <input
+            type="number"
+            id="total-salary"
+            placeholder="Enter your Salary..."
+          />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Enter Item..." id="item" />
+          <input type="number" placeholder="Enter Amount..." id="amount" />
+          <button id="add-input">Add</button>
+        </div>
+        <div class="list-container">
+          <input type="text" id="output-item" />
+          <input type="number" id="output-number" />
+        </div>
+      `;
+  inputContainer.innerHTML = inputField;
+}
