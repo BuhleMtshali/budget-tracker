34c69c3ee26019f95d8b58596b3b473d7ecc0576
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-09-08 21:46:09 +0200

    updated the stylesheet

---
+++ app.js
@@ -0,0 +1,6 @@
+let inputContainer = document.getElementById("input-content-container");
+const addBtn = document.getElementById("add-btn");
+addBtn.addEventListener("click", addItem);
+//start of function
+
+function addItem() {}
